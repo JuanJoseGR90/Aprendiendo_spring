aca70170ea58ba62cec5bd824563b24a74dd7409
JJGR90 <juanjoseguiradoruiz90@gmail.com>
2025-01-26 20:28:18 +0100

    probando a usar un modelo

--- src/main/java/com/hellrider/aprendiendo_spring/HolaController.java
+++ src/main/java/com/hellrider/aprendiendo_spring/HolaController.java
@@ -3,11 +3,21 @@ package com.hellrider.aprendiendo_spring;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class HolaController {
 
     @GetMapping("/saludo")
     public String saludo() {
         return "Hola Mundo";
+    }
+
+    @GetMapping("/user")
+    public List<User> getUser() {
+        return Arrays.asList(new User(
+                "Juan José", 18),
+                new User("Juan José", 24));
     }
 }
