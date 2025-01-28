cfd6d339acb27d907136079c98513e846ef753e6
JJGR90 <juanjoseguiradoruiz90@gmail.com>
2025-01-28 18:11:52 +0100

    he añadido dos nuevos endpoints a controller para probar a enviar los datos a través de la url y otro para obtener todo los datos guardados en la lista

--- src/main/java/com/hellrider/aprendiendo_spring/Controller.java
+++ src/main/java/com/hellrider/aprendiendo_spring/Controller.java
@@ -1,15 +1,17 @@
 package com.hellrider.aprendiendo_spring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.hellrider.aprendiendo_spring.otros.User2;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @org.springframework.stereotype.Controller
 @RequestMapping("/app")
 public class Controller {
+
+    private final List<User> users = new ArrayList<>();
 
     @ResponseBody
     @GetMapping("/saludo")
@@ -24,5 +26,18 @@ public class Controller {
                 new User("hellrider", "klfhadlkfj"),
                 new User("hellrider1990", "lkajfhaslkdfh")
         );
+    }
+
+    @ResponseBody
+    @GetMapping("/users")
+    public List<User> user2() {
+        return users;
+    }
+
+    @ResponseBody
+    @PostMapping("/crear")
+    public void addUser(@RequestParam String username, @RequestParam String password) {
+        User user = new User(username, password);
+        users.add(user);
     }
 }
